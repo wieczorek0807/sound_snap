f5526312710422156f7e31bfbfc2ebe1e922284a
MW <wieczorek0807@gmail.com>
2025-05-12 07:54:35 +0200

    clean up resources on close. Remove unused Linux and macOS files and configurations.

--- lib/features/recording_list/presentation/cubits/recording_list_cubit/recording_list_cubit.dart
+++ lib/features/recording_list/presentation/cubits/recording_list_cubit/recording_list_cubit.dart
@@ -1,10 +1,12 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:injectable/injectable.dart';
+import 'package:just_audio/just_audio.dart';
 import 'package:loggy/loggy.dart';
 import 'package:sound_snap/features/audio_player/domain/services/audio_player_service.dart';
 import 'package:sound_snap/features/recording_list/domain/entities/recording_entity/recording_entity.dart';
 import 'package:sound_snap/features/recording_list/domain/services/recording_list_service.dart';
+import 'dart:async';
 
 part 'recording_list_cubit.freezed.dart';
 part 'recording_list_state.dart';
@@ -13,8 +15,22 @@ part 'recording_list_state.dart';
 class RecordingListCubit extends Cubit<RecordingListState> with UiLoggy {
   final RecordingListService _service;
   final AudioPlayerService _audioPlayerService;
+  StreamSubscription? _playerStateSubscription;
 
-  RecordingListCubit(this._service, this._audioPlayerService) : super(const RecordingListState.initial());
+  RecordingListCubit(this._service, this._audioPlayerService) : super(const RecordingListState.initial()) {
+    _setupPlayerStateListener();
+  }
+
+  void _setupPlayerStateListener() {
+    _playerStateSubscription = _audioPlayerService.playerStateStream.listen((playerState) {
+      if (playerState.processingState == ProcessingState.completed) {
+        final currentState = state;
+        if (currentState is RecordingListStateLoaded) {
+          emit(currentState.copyWith(currentlyPlayingId: null));
+        }
+      }
+    });
+  }
 
   Future<void> loadRecordings() async {
     loggy.debug('Loading recordings initiated');
@@ -66,6 +82,7 @@ class RecordingListCubit extends Cubit<RecordingListState> with UiLoggy {
 
   @override
   Future<void> close() async {
+    await _playerStateSubscription?.cancel();
     await _audioPlayerService.dispose();
     return super.close();
   }
